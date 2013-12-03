b251f8d1f4eb4ae3d45bfba9e88ed1fff3c7c6a9
Talal Alnafie <alnafie@gmail.com>
2013-12-03 17:23:54 +0300

    fixed another bug in setup.sh

--- remote/shell/setup.sh
+++ remote/shell/setup.sh
@@ -13,13 +13,16 @@ git config --global user.email alnafie@gmail.com
 
 # nvm / node setup
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
-nvm install 0.10	#need to restart terminal after installing nvm?
+nvm install 0.10	#need to restart terminal after installing nvm? yes, figure it out -TA
 nvm alias default 0.10
 
 # nginx setup
 sudo apt-get install libpcre3 libpcre3-dev -y
 sudo wget -P /usr/src http://nginx.org/download/nginx-1.4.3.tar.gz
 cd /usr/src/ && sudo tar -xzvf nginx-1.4.3.tar.gz
+cd /usr/src/nginx-1.4.3 && sudo ./configure
+sudo make
+sudo make install
 sudo ln -s /usr/local/nginx/sbin/nginx /usr/sbin/nginx
 sudo cp /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf.default
 sudo nginx
