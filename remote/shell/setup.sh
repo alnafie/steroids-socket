0794a041c7590dce682e0cb226f6dedc3b076a34
Talal Alnafie <alnafie@gmail.com>
2013-12-03 17:06:22 +0300

    fixed setup.sh script

--- remote/shell/setup.sh
+++ remote/shell/setup.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
+# general housekeeping
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install git
-git config --global user.name "alnafie"
+sudo apt-get upgrade -y
+sudo apt-get install curl -y
+sudo apt-get install build-essential -y
+
+# git setup
+sudo apt-get install git -y
+git config --global user.name "Talal Alnafie"
 git config --global user.email alnafie@gmail.com
-sudo apt-get install curl
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh 		# nvm
-#need to restart terminal after installing nvm
-nvm install 0.10
+
+# nvm / node setup
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+nvm install 0.10	#need to restart terminal after installing nvm?
 nvm alias default 0.10
-sudo apt-get build-essential
-sudo apt-get install nginx
-sudo service nginx start #localhost:8080 working
-# update-rc.d nginx defaults		#optional to add nginx to startup
+
+# nginx setup
+sudo apt-get install libpcre3 libpcre3-dev -y
+sudo wget -P /usr/src http://nginx.org/download/nginx-1.4.3.tar.gz
+cd /usr/src/ && sudo tar -xzvf nginx-1.4.3.tar.gz
+sudo ln -s /usr/local/nginx/sbin/nginx /usr/sbin/nginx
+sudo cp /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf.default
+sudo nginx
+
+# redis setup
 sudo apt-get install redis-server
 sudo cp /etc/redis/redis.conf /etc/redis/redis.conf.default #to keep copy of redis .conf
-sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.default #same for nginx
-sudo rm /etc/nginx/sites-enabled/default
