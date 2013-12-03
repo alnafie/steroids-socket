0620c8a439f7f99554f12c6ef764250be206075d
Talal Alnafie <alnafie@gmail.com>
2013-12-03 14:00:32 +0300

    no errors but not writing

--- remote/socket.js
+++ remote/socket.js
@@ -1,14 +1,15 @@
 var io = require('socket.io').listen(3000);
+	// io.set('resource', 'msg/socket.io');
 
 io.sockets.on('connection', function (socket) {
 
   var socket = socket;
   
-  io.sockets.emit('news', {sockets: io.sockets.manager.connected});
+  io.sockets.emit('news', {hello: "world"});
 
-  socket.on('chat', function (data) {
-  	data.user = socket.id;
-  	socket.broadcast.emit('echo', data);
-  });
+  // socket.on('chat', function (data) {
+  // 	data.user = socket.id;
+  // 	socket.broadcast.emit('echo', data);
+  // });
 
 });
